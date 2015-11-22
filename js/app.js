9c1e2591aed1644429941909e4c10047754b9b67
Prathamesh <prathprabhu@gmail.com>
2015-11-22 16:58:33 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function () {                      //IIFE created for execution
+ 
+    var player = function(name,score){ // player function created.
+    this.name = name;
+    this.score = score;
+}
+
+var player1 = new player("prath",0);
+    
+    
+    
+    
+})();
